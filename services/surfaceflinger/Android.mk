9855c29d222b9c08c1c4be932e9cc80a74c3129d
Steve Kondik <steve@cyngn.com>
2015-10-12 13:44:11 -1000

    surfaceflinger: Clean up use of QC extended API
    Change-Id: I1256605e63e90f3baaa213453246fb0e44e0c770

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -106,14 +106,13 @@ LOCAL_SHARED_LIBRARIES := \
     libgui \
     libpowermanager
 
-LOCAL_WHOLE_STATIC_LIBRARIES += libexsurfaceflinger
-
-LOCAL_C_INCLUDES += $(ANDROID_BUILD_TOP)/vendor/qcom/opensource/display-frameworks/native/services/surfaceflinger/
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
+    LOCAL_WHOLE_STATIC_LIBRARIES += libexsurfaceflinger
+    LOCAL_C_INCLUDES += vendor/qcom/opensource/display-frameworks/native/services/surfaceflinger
+    LOCAL_C_INCLUDES += vendor/qcom/opensource/display-frameworks/include
     LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
     LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
-    LOCAL_C_INCLUDES += vendor/qcom/opensource/display-frameworks/include
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQTI_BSP
 endif
